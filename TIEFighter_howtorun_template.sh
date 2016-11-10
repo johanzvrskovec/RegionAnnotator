75e2dd94ecd5ebe6388c47e9e1f6cd217379393f
johkal <johan.kallberg@ki.se>
2016-11-10 12:38:31 +0100

    howtorun-templates

---
+++ TIEFighter_howtorun_template.sh
@@ -0,0 +1,4 @@
+cd /home/pgcbioif/basicAnnotation
+#sh TIEFighter.sh -input "inputGene/gencode.genes.txt" -gene -iformat TSV
+#sh TIEFighter.sh -input inputReference -reference -iformat TSV
+#sh TIEFighter.sh -input "input/pgcmdd2.txt" -iformat TSV -oformat EXCEL
