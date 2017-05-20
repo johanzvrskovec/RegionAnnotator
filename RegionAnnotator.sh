40ef3ef72f258576124a7765464b0c0dbbbe2db7
johkal <johan.kallberg@ki.se>
2017-05-20 11:17:11 +0200

    dependencies. sh config.
    new reference data a 2017-05-20

--- RegionAnnotator.sh
+++ RegionAnnotator.sh
@@ -1,2 +1,5 @@
-cd /home/pgcbioif/basicAnnotation
-java -XX:MaxHeapSize=1024m -jar "RegionAnnotator.jar" $*
+#set your program location dir here
+#dir=/some/program/dir
+#for example
+#dir=/nas/depts/007/sullilab/shared/bioinf/regionAnnotator1.6.0
+java -XX:MaxHeapSize=1024m -jar "$dir/RegionAnnotator.jar" $*
